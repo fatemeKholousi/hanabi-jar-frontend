c3b3377deb1a2f8f0c6450945e1d5c36d23bb255
Fateme kholousi <fatemekholousizaer@gmail.com>
2022-03-04 19:45:26 +0330

    icon and router package added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/login-user" element={<Login />} />
+        {/*   <Route path="ashaar/page2" element={<Ashaar2 />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
