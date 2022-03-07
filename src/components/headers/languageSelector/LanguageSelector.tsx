322d3b17cea9d51663f4c68824197978d8befba3
Fateme kholousi <fatemekholousizaer@gmail.com>
2022-03-07 20:44:10 +0330

    fix: i18n with typescript installed and it works

---
+++ src/components/headers/languageSelector/LanguageSelector.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button,Switch } from "antd";
+import i18n from "i18next";
+import { useDispatch, useSelector } from "../../../utils//hooks";
+import { ChangeAppLanguageAction } from "../../../utils/i18n/i18n.slice";
+
+const LanguageSelector = () => {
+  const { appLanguage } = useSelector((state:any) => state.appLanguageReducer);
+  const dispatch = useDispatch();
+
+  const handleChange = (value: "fa" | "en") => {
+    if (value === "en") {
+      i18n.changeLanguage("fa");
+      dispatch(ChangeAppLanguageAction("fa"));
+    } else {
+      i18n.changeLanguage("en");
+      dispatch(ChangeAppLanguageAction("en"));
+    }
+  };
+
+  return (
+    // <Button
+    //   type="primary"
+    //   onClick={}
+    //   className="language-selector"
+    // >
+    //   {appLanguage === "en" ? "Persian" : "English"}
+    // </Button>
+
+
+<Switch
+onClick={() => handleChange(appLanguage)}
+onChange={ (checked: boolean) =>{
+  console.log(`switch to ${checked}`);
+}}
+checkedChildren="Persian"
+unCheckedChildren="English"
+defaultChecked
+/>
+
+
+  );
+};
+
+export default LanguageSelector;
