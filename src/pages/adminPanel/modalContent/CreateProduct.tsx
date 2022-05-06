ca9278f34b1278e45ae274874eede0314cd31cbd
fatemekholousi <fatemekholousizaer@gmail.com>
2022-05-06 17:59:54 +0430

    fix: add forwardRef as practice

--- src/pages/adminPanel/modalContent/CreateProduct.tsx
+++ src/pages/adminPanel/modalContent/CreateProduct.tsx
@@ -1,9 +1,71 @@
-import React from "react";
+import React, { useRef, useImperativeHandle, forwardRef } from "react";
 import { Form, Input, InputNumber, Modal, Select } from "antd";
+
+export const ContentCreateProduct = forwardRef((props: any, forwardedRef) => {
+  const { onUploadImage } = props;
+  const { TextArea } = Input;
+  const localRefInput = useRef<any>();
+
+  useImperativeHandle(forwardedRef, () => {
+    return {
+      focusAndBlur: () => {
+        localRefInput.current.focus();
+        setTimeout(() => {
+          localRefInput.current.blur();
+        }, 10000);
+      },
+    };
+  });
+
+  return (
+    <Form>
+      <Form.Item style={{ width: "100%", display: "flex" }}>
+        <Form.Item label="عنوان کتاب">
+          <input type="text" style={{ width: "40%" }} />
+        </Form.Item>
+        <Form.Item label="نام نویسنده">
+          <input type="text" style={{ width: "40%" }} ref={localRefInput} />
+        </Form.Item>
+      </Form.Item>
+
+      <Form.Item label="سال انتشار ">
+        <Input style={{ width: "40%" }} />
+      </Form.Item>
+      <Form.Item label="ژانر">
+        <Select>
+          <Select.Option value="demo">Demo</Select.Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item label="کاور محصول">
+        <input
+          type="file"
+          onChange={onUploadImage}
+          accept="image/png, image/jpeg"
+        />
+      </Form.Item>
+      <div id="image-grid" />
+
+      <Form.Item label="موجودی">
+        <InputNumber />
+      </Form.Item>
+      <Form.Item label="چکیده داستان">
+        <TextArea
+          showCount
+          maxLength={100}
+          style={{ height: 120 }}
+          onChange={(e) => {
+            console.log("Change:", e.target.value);
+          }}
+        />
+      </Form.Item>
+    </Form>
+  );
+});
 
 const CreateProduct = (props: any) => {
   const { isModalVisible, onShowModal, onCancle } = props;
-  const { TextArea } = Input;
+  const inputRef = useRef<any>();
 
   const reader = new FileReader();
   const img: any = document.createElement("img");
@@ -26,52 +88,8 @@ const CreateProduct = (props: any) => {
       }
     });
   }
-
-  function content() {
-    return (
-      <Form>
-        <Form.Item style={{ width: "100%", display: "flex" }}>
-          <Form.Item label="عنوان کتاب">
-            <Input style={{ width: "40%" }} />
-          </Form.Item>
-          <Form.Item label="نام نویسنده">
-            <Input style={{ width: "40%" }} />
-          </Form.Item>
-        </Form.Item>
-
-        <Form.Item label="سال انتشار ">
-          <Input style={{ width: "40%" }} />
-        </Form.Item>
-        <Form.Item label="ژانر">
-          <Select>
-            <Select.Option value="demo">Demo</Select.Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item label="کاور محصول">
-          <input
-            type="file"
-            onChange={handleUploadImage}
-            accept="image/png, image/jpeg"
-          />
-        </Form.Item>
-        <div id="image-grid" />
-
-        <Form.Item label="موجودی">
-          <InputNumber />
-        </Form.Item>
-        <Form.Item label="چکیده داستان">
-          <TextArea
-            showCount
-            maxLength={100}
-            style={{ height: 120 }}
-            onChange={(e) => {
-              console.log("Change:", e.target.value);
-            }}
-          />
-        </Form.Item>
-      </Form>
-    );
+  if (isModalVisible && inputRef) {
+    inputRef?.current?.focusAndBlur();
   }
 
   return (
@@ -81,7 +99,7 @@ const CreateProduct = (props: any) => {
       onOk={onShowModal}
       onCancel={onCancle}
     >
-      {content()}
+      <ContentCreateProduct onUploadImage={handleUploadImage} ref={inputRef} />
     </Modal>
   );
 };
